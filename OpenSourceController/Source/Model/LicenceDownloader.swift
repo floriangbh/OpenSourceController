9e9bf56fba505a48ee900cbc6decff6325db79e8
Florian Gabach <florian.gabach@gmail.com>
2017-02-27 17:07:34 +0100

    First commit

---
+++ OpenSourceController/Source/Model/LicenceDownloader.swift
@@ -0,0 +1,38 @@
+//
+//  LicenceDownloader.swift
+//  Pods
+//
+//  Created by Florian Gabach on 21/02/2017.
+//
+//
+
+import UIKit
+
+class LicenceDownloader: NSObject {
+
+    /// Download every licence 
+    ///
+    /// - Parameters:
+    ///   - licences: array which contains licence model model 
+    ///   - completion: end downloading completion
+    class func downloadLicences(licences: [LicenceFile],
+                                completion: @escaping () -> Void) {
+        // Number of licences 
+        let licenceCount = licences.count
+
+        // Already licence downloaded 
+        var alreadyDownloaded = 0
+
+        // Download licence's detail for every lience 
+        for licence in licences {
+            licence.downloadLicenceDetail(completion: { () in
+                alreadyDownloaded += 1
+
+                // If this is the last licence, perfom completion
+                if licenceCount == alreadyDownloaded {
+                    completion()
+                }
+            })
+        }
+    }
+}
