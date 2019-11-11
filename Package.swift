dca1bd740d279234e49c2c792a579319edc45b79
Florian Gabach <florian.gabach@gmail.com>
2019-11-11 17:19:43 +0100

    Fix SMP file

--- Package.swift
+++ Package.swift
@@ -1,28 +1,18 @@
 // swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "OpenSourceController",
+    platforms: [.iOS(.v10)],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "OpenSourceController",
-            targets: ["OpenSourceController"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .library(name: "OpenSourceController", targets: ["OpenSourceController"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "OpenSourceController",
-            dependencies: []),
-        .testTarget(
-            name: "OpenSourceControllerTests",
-            dependencies: ["OpenSourceController"]),
-    ]
+      .target(
+        name: "OpenSourceController",
+        path: "",
+        sources: ["Sources"]
+      )
+    ],
+    swiftLanguageVersions: [.v4, .v5]
 )
